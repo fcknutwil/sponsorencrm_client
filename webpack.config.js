250c0d723f2b9da983de26c09dbd0d0260da2963
Arnold Marcel (U116399) <marcel.arnold@mobi.ch>
2017-08-31 11:09:40 +0200

    SCSS-Support zu Webpack hinzugefügt

--- webpack.config.js
+++ webpack.config.js
@@ -11,6 +11,17 @@ const config = {
             {
                 test: /\.ts$/,
                 use: 'ts-loader'
+            }, {
+                test: /\.scss$/,
+                use: [
+                    {
+                        loader: "style-loader" // creates style nodes from JS strings
+                    }, {
+                        loader: "css-loader" // translates CSS into CommonJS
+                    }, {
+                        loader: "sass-loader" // compiles Sass to CSS
+                    }
+                ]
             }
         ]
     }
