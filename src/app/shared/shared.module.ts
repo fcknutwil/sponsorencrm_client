abb9a41e6ddfd761570fb2d40d0f63b31edb5a12
Arnold Marcel (U116399) <marcel.arnold@mobi.ch>
2017-11-29 19:03:55 +0100

    remove build failures

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,7 +1,7 @@
 import {NgModule} from "@angular/core";
 import {CommonModule} from "@angular/common";
 import {FormsModule} from "@angular/forms";
-import {MdInputModule, MdButtonModule} from "@angular/material";
+import {MatButtonModule, MatInputModule} from "@angular/material";
 import {SessionService} from "./session.service";
 import {AuthGuard} from "./auth-guard.service";
 
@@ -9,16 +9,16 @@ import {AuthGuard} from "./auth-guard.service";
     imports: [
         CommonModule,
         FormsModule,
-        MdButtonModule,
-        MdInputModule
+        MatButtonModule,
+        MatButtonModule
     ],
     declarations: [],
     providers: [SessionService, AuthGuard],
     exports: [
         CommonModule,
         FormsModule,
-        MdButtonModule,
-        MdInputModule
+        MatButtonModule,
+        MatInputModule
     ]
 })
 export class SharedModule {
