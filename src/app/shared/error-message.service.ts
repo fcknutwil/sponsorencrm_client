6d828096a9030e40bbd72a12bbaae74fda7367cc
maesi <mail@maesi.org>
2018-01-20 00:32:36 +0100

    Error Message anzeigen

--- src/app/shared/error-message.service.ts
+++ src/app/shared/error-message.service.ts
@@ -4,13 +4,16 @@ import {Injectable} from "@angular/core";
 import {MatSnackBar} from "@angular/material";
 import {HttpErrorResponse} from "@angular/common/http";
 
+import * as _ from "lodash";
+
 @Injectable()
 export class ErrorMessageService {
 
     constructor(public snackBar: MatSnackBar) {}
 
     public logHttpError(error: HttpErrorResponse): void {
-        this.snackBar.open(error.message, "Schliessen", {
+        const message = _.get(error, 'error.message', error.message);
+        this.snackBar.open(message, "Schliessen", {
             duration: 3000
         });
     }
